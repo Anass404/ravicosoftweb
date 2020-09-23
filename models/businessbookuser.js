21b6a5a80cdb96178154a49cfbf2c25d82198ffb
Atta Rasul <attachaudhury@gmail.com>
2020-09-23 21:25:17 +0500

    changes

--- models/businessbookuser.js
+++ models/businessbookuser.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-//const bcrypt = require('bcrypt-nodejs');
 const validator = require('mongoose-unique-validator');
 
     const businessbookuser = mongoose.Schema({
         createddate:{type:Date,required:false,default:new Date()},
         membershiptype:{type:String,default:'free'},
         membershipexpirydate:Date,
+        username:{type:String,required:true,default:Date.now().toString()},
         password:{type:String,required:false,default:Date.now().toString()},
         softwareshouldrun:{type:Boolean,default:true},
         cansendsms:{type:Boolean,default:false},
